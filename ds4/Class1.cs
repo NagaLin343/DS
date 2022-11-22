706ba145c586a082e17500feaf45a5bf506b6147
NagaLin343 <i.ibragimova@xlab-game.com>
2022-11-22 23:27:29 +0300

    DS4//22/11/22

---
+++ ds4/Class1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DS4
+{
+    public class LinkedList<TItem> : IEnumerable<TItem>
+    {
+         public class Item<TItem>
+        {
+            public TItem Data { get; set; }
+            public Item<TItem> Next { get; set; }
+            public Item(TItem data)
+            {
+                if (data == null)
+                {
+                    throw new ArgumentNullException(nameof(data));
+                }
+
+                Data = data;
+            }
+            public override string ToString()
+            {
+                return Data.ToString();
+            }
+        }
+        private Item<TItem> _head = null;
+
+        private Item<TItem> _tail = null;
+
+        private int _count = 0;
+        public int Count
+        {
+            get => _count;
+        }
+
+        public void Add(TItem data)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+            var item = new Item<TItem>(data);
+            if (_head == null)
+            {
+                _head = item;
+            }
+            else
+            {
+                _tail.Next = item;
+            }
+            _tail = item;
+
+            _count++;
+        }
+
+        public void Delete(TItem data)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+            var current = _head;
+            Item<TItem> previous = null;
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    
+                    if (previous != null)
+                    {
+                        previous.Next = current.Next;
+
+                        if (current.Next == null)
+                        {
+                            _tail = previous;
+                        }
+                    }
+                    else
+                    {
+                        _head = _head.Next;
+                        if (_head == null)
+                        {
+                            _tail = null;
+                        }
+                    }
+                    _count--;
+                    break;
+                }
+                previous = current;
+                current = current.Next;
+            }
+        }
+        public void Clear()
+        {
+            _head = null;
+            _tail = null;
+            _count = 0;
+        }
+        public IEnumerator<TItem> GetEnumerator()
+        {
+            var current = _head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ ds4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace DS4
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+                var list = new LinkedList<int>();
+            for (int i = 0; i <= 4; i++)
+            {
+                Console.Write($"Введите {i+1} элемент: ");
+                int k = Convert.ToInt32(Console.ReadLine());
+                list.Add(k);
+            }
+                foreach (var item in list)
+                {
+                    Console.WriteLine(item);
+                }
+         
+                Console.Write($"Введите индекс элемент: ");
+                int index = Convert.ToInt32(Console.ReadLine());
+                list.Delete(index);
+
+                foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+
+                list.Clear();
+                foreach (var item in list)
+                {
+                    Console.WriteLine(item);
+                }
+        }
+        }
+    
+}
