2e7ce2b49dd419d46e2c8ba680f7df7175ad5cf0
NagaLin343 <i.ibragimova@xlab-game.com>
2022-11-22 22:33:40 +0300

    22/11/22

---
+++ ds3NEW/List.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+namespace DS3
+{
+    internal class List
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> myList = new MyList<int>();
+
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.Write($"Введите {i} элемент: ");
+                int k = Convert.ToInt32(Console.ReadLine());
+                myList.Add(k);
+            }
+
+            Console.Write($"Введите индекс элемент: ");
+            int index = Convert.ToInt32(Console.ReadLine());
+            myList.RemoveAt(index);
+
+            foreach (int i in myList)
+            {
+                Console.WriteLine(i);
+            }
+
+            myList.Clear();
+
+            Console.WriteLine("////////////////////////");
+
+            foreach (int i in myList)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("////////////////////////");
+
+
+        }
+    }
+}
---
+++ ds3NEW/MyList.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+namespace DS3
+{
+    public class MyList<TItem> : IEnumerable<TItem>
+    {
+        private TItem[] array = new TItem[count];
+
+        private static int count = 5;
+        public int Count { get => count; }
+
+        int length = 0;
+
+
+        public void Add(TItem item)
+        {
+
+            if (length < count)
+            {
+                array[length] = item;
+                length++;
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+
+            for (int i = index; i < count - 1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+
+            count--;
+            Array.Resize(ref array, count);
+
+        }
+
+        public void Remove(TItem item)
+        {
+
+            for (int i = 0; i < count; i++)
+            {
+                if (array[i].Equals(item))
+                    array[i] = array[i + 1];
+            }
+
+            count--;
+            Array.Resize(ref array, count);
+
+        }
+
+        public void Clear()
+        {
+            array = new TItem[count];
+        }
+
+
+        public IEnumerator<TItem> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return array[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
